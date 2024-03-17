d61a5f931412f6964d18fbad7b4229d85f86b796
Herika Neves <159446653+herika-dev@users.noreply.github.com>
2024-03-17 11:36:39 -0300

    Update 0 Responda o seguinte questionario.py

--- 0 Responda o seguinte questionario.py
+++ 0 Responda o seguinte questionario.py
@@ -1,6 +1,9 @@
- Utilizando o Python e Pandas através do Visual Studio, irei responder as demais questões solicitadas. Nelas estarão todos os comandos utilizados para obterás informações solicitadas da tabela que foi fornecida em anexo, com algumas hashtag descrevendo o código para melhor interpretação.
+ Utilizando o Python e Pandas através do Visual Studio, irei responder as demais questões solicitadas. Nelas estarão todos os comandos utilizados para obterás informações solicitadas da tabela que foi fornecida em anexo,
+com algumas hashtag descrevendo o código para melhor interpretação.
 
- A estatística nos ajuda a entender e interpretar grandes volumes de dados, através de tabelas resumo, medidas de dispersão e posição, gráficos e outras ferramentas que serão apresentadas ao longo do curso. Utilizando a base de dados disponibilizada no link abaixo, vamos analisar a base de remuneração dos servidores públicos do Distrito Federal, compreendendo o comportamento da mesma. A base foi obtida no portal da transparência e modificada, retirando os nomes e parte do CPF.
+ A estatística nos ajuda a entender e interpretar grandes volumes de dados, através de tabelas resumo, medidas de dispersão e posição, gráficos e outras ferramentas que serão apresentadas ao longo do curso. Utilizando 
+a base de dados disponibilizada no link abaixo, vamos analisar a base de remuneração dos servidores públicos do Distrito Federal, compreendendo o comportamento da mesma. A base foi obtida no portal da transparência e
+modificada, retirando os nomes e parte do CPF.
 https://drive.google.com/file/d/1UrlwBHa47H5lpAkLtDsfuEWfTMQj491X/view?usp=sharing
 
 Qual o espaço amostral compreendido nesta base de dados?
