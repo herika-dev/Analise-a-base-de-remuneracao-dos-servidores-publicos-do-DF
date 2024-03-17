4a6a2c4fc958edce36d25fd543c9c0c91a42d4a8
Herika Neves <159446653+herika-dev@users.noreply.github.com>
2024-03-17 11:35:31 -0300

    Update 0 Responda o seguinte questionario.py
    Consulta de dados de sobre a remuneração dos Servidores Públicos DF

--- 0 Responda o seguinte questionario.py
+++ 0 Responda o seguinte questionario.py
@@ -1,10 +1,29 @@
- #Utilizando o Python e Pandas através do Visual Studio, irei responder as demais questões solicitadas.
- #Nelas estarão todos os comandos utilizados para obterás informações solicitadas da tabela que foi fornecida em anexo,
-#com algumas hashtag descrevendo o código para melhor interpretação.
+ Utilizando o Python e Pandas através do Visual Studio, irei responder as demais questões solicitadas. Nelas estarão todos os comandos utilizados para obterás informações solicitadas da tabela que foi fornecida em anexo, com algumas hashtag descrevendo o código para melhor interpretação.
 
- #A estatística nos ajuda a entender e interpretar grandes volumes de dados, através de tabelas resumo, medidas de dispersão e posição, 
-#gráficos e outras ferramentas que serão apresentadas ao longo do curso. Utilizando a base de dados disponibilizada no link abaixo, vamos 
-#analisar a base de remuneração dos servidores públicos do Distrito Federal, compreendendo o comportamento da mesma. A base foi obtida no 
-#portal da transparência e modificada, retirando os nomes e parte do CPF.
-#https://drive.google.com/file/d/1UrlwBHa47H5lpAkLtDsfuEWfTMQj491X/view?usp=sharing
+ A estatística nos ajuda a entender e interpretar grandes volumes de dados, através de tabelas resumo, medidas de dispersão e posição, gráficos e outras ferramentas que serão apresentadas ao longo do curso. Utilizando a base de dados disponibilizada no link abaixo, vamos analisar a base de remuneração dos servidores públicos do Distrito Federal, compreendendo o comportamento da mesma. A base foi obtida no portal da transparência e modificada, retirando os nomes e parte do CPF.
+https://drive.google.com/file/d/1UrlwBHa47H5lpAkLtDsfuEWfTMQj491X/view?usp=sharing
 
+Qual o espaço amostral compreendido nesta base de dados?
+1● Quantos servidores estão lotados no corpo de bombeiros?
+2● Qual o órgão público com maior número de funcionários?
+3● Qual a probabilidade de ao escolher uma linha ao acaso, ser de um
+funcionário que trabalha na secretaria de educação?
+
+4● Liste todas as funções contidas na base
+5● Qual órgão publico possui a maior média salarial?
+6● O servidor com maior remuneração básica pertence a qual órgão?
+7● Qual o valor pago para todos os funcionários públicos?
+
+8● Elabore um gráfico de pizza com a remuneração bruta dos servidores por
+9orgão
+10● Elabore um gráfico de boxplot com a situação do servidor e sua remuneração
+liquida
+11● Identifique qual órgão possui salário liquido com menos variações, utilizando
+para isto medidas de dispersão.
+12● Calcule o índice de correlação entre o IRRF e os salários liquido e bruto. Em
+qual dos casos o índice de correlação foi maior?
+13● Adicione uma nova coluna que irá conter a diferença entre o salario bruto e
+liquido, e responda as seguintes questões;
+13.1o Qual a correlação entre o IRRF pago e esta nova coluna?
+13.2o Qual órgão apresenta índice de correlação entre IRRF e diferença
+salarial maior?
